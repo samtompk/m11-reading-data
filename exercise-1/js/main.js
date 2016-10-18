743a26b9fac1af6a6ba87c1d13da04288e424721
Sam <samtompk@gmail.com>
2016-10-18 10:01:06 -0700

    exercise complete

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -3,7 +3,33 @@
 // You'll have to wait for you page to load to assign events to the elements created in your index.html file
 $(function() {
     // Use $.get to read in your `data/students.csv` dataset: remember, you must be running a local server
+    $.get('data/students.csv', function(data, error) {
+    	var parsedData = Papa.parse(data, {
+    		header:true
+    	}).data;
 
+    	//console.log(parsedData);
+
+    	var table = $('<table>');
+    	table.addClass('table');
+
+    	Object.keys(parsedData[0]).forEach(function(d) {
+    		table.append($('<th>' + d + '</th>'))
+    	});
+       	//console.log(tableHeaders);
+
+       	parsedData.forEach(function(d) {
+       		var tr = $('<tr>');
+
+       		Object.keys(d).forEach(function(dd) {
+       			tr.append($('<td>' + d[dd] + '</td>'));
+       		})
+
+       		table.append(tr);
+       	});
+
+    	$('#sandbox').append(table);
+    });
         // Parse the data using Papa.parse
 
 
@@ -21,5 +47,5 @@ $(function() {
 
         // Select your `sandbox` section, and append your table to it
 
-    });
+
 });
